13e6a8fcd04e6592b35aa38e02230775f9e1b052
Masayoshi-Nakagawa <102711095+Masayoshi-Nakagawa@users.noreply.github.com>
2024-01-31 18:16:08 +0900

    Update navigation1.py

--- navigation1.py
+++ navigation1.py
@@ -2,6 +2,18 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 st.markdown("""
+<!DOCTYPE html>
+<html lang="jp" style="scroll-padding-top:100px;">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novel.+</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">    
+</head>
+
 <body>
     <!-- NAV -->
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
@@ -36,7 +48,8 @@ st.markdown("""
         });
     </script> 
     
-</body>""",unsafe_allow_html=True)
+</body>
+</html>""",unsafe_allow_html=True)
 
 
 
