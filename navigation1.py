abe35ec1f68e13a76ad480caf227764a635bf362
Masayoshi-Nakagawa <102711095+Masayoshi-Nakagawa@users.noreply.github.com>
2024-01-31 18:21:18 +0900

    Update navigation1.py

--- navigation1.py
+++ navigation1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-image = image.open('/mount/src/webapp/logo_novel1.png')
+image = image.open('logo_novel1.png')
 st.image(image,widt=200)
 
 
